0ddf2d5116a9abe265573d3c1fd14cd2722178e7
tilen.okretic <tilen.okretic@gmail.com>
2025-08-03 20:47:47 +0200

    feat: readme this is

---
+++ README.d
@@ -0,0 +1,12 @@
+
+# Raylib Portable
+
+A starter template for my raylib projects.
+
+This project builds Raylib froum source code (Raylib 5.6)
+
+---
+
+## References
+
+* raylib: `https://www.raylib.com/`
